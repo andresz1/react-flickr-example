d91c5d098e35a7e5f7468af59d27f2d212155ede
Andres Alvarez <a.alvarez.sor@gmail.com>
2018-09-07 19:06:39 +0200

    Fix build path

--- server/config/express.js
+++ server/config/express.js
@@ -20,10 +20,10 @@ app.use(helmet());
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '../../client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
   });
 }
 
